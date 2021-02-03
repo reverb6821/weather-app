20549e239ae5b3cb0798f06904c0499e93a579bb
Giuseppe Gigliotti <giuseppegigliottidev@gmail.com>
2021-02-03 21:26:00 +0100

    starting

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import reactDOM from 'react-dom';
+import './App.scss';
+
+// ?API Key
+const apiKey = {
+  url:'https://api.openweathermap.org/data/2.5/',
+  key:'0ce3b9901c30cb494d6e5e43c1a96ae8'
+}
+
+const App = () => {
+
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${apiKey.url}weather?q=${query}&units=metric&APPID=${apiKey.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+  return (
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp >= 16) ? 'app warm' : 'app' || (weather.main.temp < 15) ? 'app cold' : 'app')  : 'app'}>
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°c
+            </div>
+            <div className="weather">{weather.weather[0].main}</div>
+          </div>
+        </div>
+        ) : ('')}
+      </main>
+    </div>
+  )
+}
+
+reactDOM.render(<App />, document.getElementById('root'))
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './src/App.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+            {
+                loader: "file-loader",
+                options: {
+                    outputPath: 'images'
+                }
+            }
+        ]
+    }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "React App",
+      template: './public/index.html'
+  })
+  ],
+  devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    port: 3000
+  }
+}
